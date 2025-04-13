8916c6bd57fdc620c3f98601ef195b46472433af
DarkRiters <bartosz.biel@studenci.collegiumwitelona.pl>
2025-04-13 20:11:37 +0200

    Chanes with configuration, fixed errors new env's, project works!

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string
+  readonly VITE_OTHER_API_KEY: string
+  // dodaj więcej jak potrzeba
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
